0b783e812e8a6244097d4b5108b5c46ed0039da8
MOBSkuchen <71943003+MOBSkuchen@users.noreply.github.com>
2025-02-16 14:27:36 +0100

    Add command line parser

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,13 @@
 use std::fs::File;
-use std::io::{BufReader, Cursor, ErrorKind};
+use std::io::{BufReader, ErrorKind};
+use clap::{Arg, ColorChoice, ValueHint};
 use image::{ImageFormat, ImageReader};
 
+pub const NAME: &str = env!("CARGO_PKG_NAME");
+pub const VERSION: &str = env!("CARGO_PKG_VERSION");
+pub const DESCRIPTION: &str = env!("CARGO_PKG_DESCRIPTION");
+
+#[derive(Debug)]
 enum ImcoError {
     // IO Errors; Reason, Path
     FailedFileRead(String, String),
@@ -34,7 +40,7 @@ fn io_error_convert<T, E>(res: Result<T, std::io::Error>, file_path: &str, is_re
 }
 
 fn imread(path: &str) -> ImcoResult<ImReader> {
-    io_error_convert(ImageReader::open(path), path, true)
+    io_error_convert::<ImReader, ImcoError>(ImageReader::open(path), path, true)
 }
 
 fn mk_format(f: &String) -> ImcoResult<ImageFormat> {
@@ -42,5 +48,76 @@ fn mk_format(f: &String) -> ImcoResult<ImageFormat> {
 }
 
 fn main() -> Result<(), ImcoError> {
+    let matches = clap::Command::new(NAME)
+        .about(DESCRIPTION)
+        .version(VERSION)
+        .color(ColorChoice::Never)
+        .disable_version_flag(true)
+        .arg(Arg::new("version")
+            .short('v')
+            .long("version")
+            .help("Displays the version")
+            .action(clap::ArgAction::Version))
+        .arg(Arg::new("input")
+            .help("Input files (seperated by ',')")
+            .short('i')
+            .long("input")
+            .required(true)
+            .value_delimiter(',')
+            .value_hint(ValueHint::AnyPath)
+            .value_name("FILE")
+            .action(clap::ArgAction::Set))
+        .arg(Arg::new("output")
+            .help("Output files (seperated by ',')")
+            .short('o')
+            .long("output")
+            .required(true)
+            .value_delimiter(',')
+            .value_hint(ValueHint::AnyPath)
+            .value_name("FILE")
+            .action(clap::ArgAction::Set))
+        .arg(Arg::new("input-format")
+            .help("Input files formats (see below)")
+            .short('f')
+            .long("input-format")
+            .value_name("FORMAT")
+            .action(clap::ArgAction::Set))
+        .arg(Arg::new("output-format")
+            .help("Output files formats (see below)")
+            .short('d')
+            .long("output-format")
+            .value_name("FORMAT")
+            .action(clap::ArgAction::Set))
+        .arg(Arg::new("batch")
+            .help("Enables batch processing (using patterns to specify multiple files at once)")
+            .short('b')
+            .long("batch")
+            .action(clap::ArgAction::SetTrue))
+        .get_matches();
+    
+    let mut couples = vec![];
 
+    let input_files: Vec<&String> = matches
+        .get_many::<String>("input")
+        .unwrap()
+        .collect();
+
+    let output_files: Vec<&String> = matches
+        .get_many::<String>("output")
+        .map(|values| values.collect())
+        .unwrap_or_default();
+
+    for (i, input_file) in input_files.iter().enumerate() {
+        let partner = if output_files.is_empty() {
+            None
+        } else if i >= output_files.len() {
+            Some(output_files.last().unwrap())
+        } else {
+            Some(&output_files[i])
+        };
+        
+        couples.push((input_file, partner))
+    }
+
+    Ok(())
 }
