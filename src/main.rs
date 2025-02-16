68e31f278e703b55e936c52879c21725ec22097a
MOBSkuchen <71943003+MOBSkuchen@users.noreply.github.com>
2025-02-16 17:55:18 +0100

    Add batching

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,11 @@
-use std::ffi::OsStr;
 use std::fmt;
 use std::fs::File;
 use std::io::{BufReader, ErrorKind};
 use clap::{Arg, ArgMatches, ColorChoice, ValueHint};
+use clap::parser::ValuesRef;
 use image::{ImageError, ImageFormat, ImageReader};
 use image::error::{UnsupportedError, UnsupportedErrorKind};
+use glob::glob;
 
 pub const NAME: &str = env!("CARGO_PKG_NAME");
 pub const VERSION: &str = env!("CARGO_PKG_VERSION");
@@ -15,18 +16,19 @@ enum ImcoError {
     // IO Errors; Reason, Path
     FailedFileRead(String, String),
     FailedFileWrite(String, String),
-    
+    InvalidBatching,
     // Format
     InvalidFormat(String),
-    
     NoDestFormat,
-    
     // file path, [hint]
     Decoding(String, String),
     Encoding(String, String),
     Unsupported(String, String),
     InternalConversionError(String),
     ResourceLimitReached(String),
+    // Error, Pattern
+    BatchPattern(String, String),
+    BatchReadEntry(String)
 }
 
 impl fmt::Display for ImcoError {
@@ -35,12 +37,15 @@ impl fmt::Display for ImcoError {
             ImcoError::FailedFileRead(cause, path) => {write!(f, "Failed reading '{path}' => {cause}")}
             ImcoError::FailedFileWrite(cause, path) => {write!(f, "Failed writing '{path}' => {cause}")}
             ImcoError::InvalidFormat(fmt) => {write!(f, "Unknown format {fmt}, use --help for a list")}
+            ImcoError::InvalidBatching => {write!(f, "Batching is only allowed when specifying an output format (using --output-format)")}
             ImcoError::NoDestFormat => {write!(f, "No output format provided (use --output-format)")}
             ImcoError::Decoding(path, hint) => {write!(f, "Error during decoding of '{path}' => {hint}")}
             ImcoError::Encoding(path, hint) => {write!(f, "Error during encoding of '{path}' => {hint}")}
             ImcoError::Unsupported(path, hint) => {write!(f, "{hint} during conversion of '{path}'")}
             ImcoError::InternalConversionError(path) => {write!(f, "Internal error during conversion of '{path}'")}
-            ImcoError::ResourceLimitReached(path) => {write!(f, "Exceeded resource limitation during conversion of '{path}'")}
+            ImcoError::ResourceLimitReached(path) => {write!(f, "Exceeded resource limitation during conversion of '{path}'")},
+            ImcoError::BatchPattern(err, pat) => { write!(f, "Failed to collect files using glob, '{pat}' => {err}") }
+            ImcoError::BatchReadEntry(err) => { write!(f, "Failed to read directory entry using glob => {err}") },
         }
     }
 }
@@ -80,17 +85,13 @@ fn mk_format_fp(f: &String) -> ImcoResult<ImageFormat> {
     ImageFormat::from_extension(std::path::Path::new(f).extension().ok_or(ImcoError::InvalidFormat(f.to_owned()))?).ok_or(ImcoError::InvalidFormat(f.to_owned()))
 }
 
-fn only_filename(f: &String) -> Option<&OsStr> {
-    std::path::Path::new(f).file_stem()
-}
-
 fn mk_unsupported_str(u: UnsupportedError) -> String {
     match u.kind() {
         UnsupportedErrorKind::Color(c) => {
             format!("Unsupported color ({:?})", c)
         }
         UnsupportedErrorKind::Format(f) => {
-            format!("Unsupported image format or not allowed format ({})", f)
+            format!("Unsupported or not allowed image format ({})", f)
         }
         UnsupportedErrorKind::GenericFeature(gf) => {
             gf
@@ -112,28 +113,33 @@ fn image_err_convert<T>(res: Result<T, ImageError>, img_path: String) -> Result<
     })
 }
 
-fn individual_process(path: String, output: Option<String>, i_fmt: Option<ImageFormat>, o_fmt: Option<ImageFormat>) -> ImcoResult<(String, Option<ImageFormat>, ImageFormat)> {
+fn mk_filename(p: &String, fmt: ImageFormat) -> String {
+    std::path::Path::new(&p).file_stem()
+        .or(Some((p.clone() + fmt.extensions_str()[0]).as_ref()))
+        .unwrap().to_str().unwrap().to_string()
+}
+
+fn join_path(p: &String, fmt: ImageFormat, stem: &String) -> String {
+    std::path::Path::new(stem).join(mk_filename(p, fmt)).to_str().unwrap().to_string()
+}
+
+fn individual_process(path: String, output: Option<String>, i_fmt: Option<ImageFormat>, o_fmt: Option<ImageFormat>, batch: bool) -> ImcoResult<(String, Option<ImageFormat>, ImageFormat)> {
     if output.is_none() && o_fmt.is_none() { return Err(ImcoError::NoDestFormat) }
     
     let mut raw_image = imread(&*path)?;
     let org_fmt = if i_fmt.is_some() {
         raw_image.set_format(i_fmt.unwrap());
         i_fmt
-    } else {
-        raw_image.format()
-    };
+    } else { raw_image.format() };
     let image = image_err_convert(raw_image.decode(), path.clone())?;
     
     Ok(if o_fmt.is_some() {
         let fmt = o_fmt.unwrap();
-        let output = if output.is_some() { output.unwrap() } else {
-            std::path::Path::new(&path).file_stem()
-                .or(Some((path.clone() + fmt.extensions_str()[0]).as_ref()))
-                .unwrap().to_str().unwrap().to_string()
-        };
+        let output = if batch { join_path(&path, fmt, &output.unwrap()) } else { if output.is_some() { output.unwrap() } else { mk_filename(&path, fmt) } };
         image_err_convert(image.save_with_format(&output, fmt), path)?;
         (output, org_fmt, fmt)
     } else {
+        if batch { return Err(ImcoError::InvalidBatching) }
         let output = output.unwrap();
         let aif = mk_format_fp(&output)?;
         image_err_convert(image.save(&output), path)?;
@@ -141,12 +147,12 @@ fn individual_process(path: String, output: Option<String>, i_fmt: Option<ImageF
     })
 }
 
-fn process(couples: Vec<(&&String, Option<&&String>)>, i_fmt_s: Option<&String>, o_fmt_s: Option<&String>, batch: bool) -> ImcoResult<()> {
+fn process(couples: Vec<(&String, Option<&&String>)>, i_fmt_s: Option<&String>, o_fmt_s: Option<&String>, batch: bool) -> ImcoResult<()> {
     let i_fmt = if i_fmt_s.is_some() { Some(mk_format(i_fmt_s.unwrap())?) } else {None};
     let o_fmt = if o_fmt_s.is_some() { Some(mk_format(o_fmt_s.unwrap())?) } else {None};
 
     for couple in couples {
-        let res = individual_process(couple.0.to_string(), couple.1.and_then(|t| { Some(t.to_string()) }), i_fmt, o_fmt)?;
+        let res = individual_process(couple.0.to_string(), couple.1.and_then(|t| { Some(t.to_string()) }), i_fmt, o_fmt, batch)?;
         if res.1.is_some() {
             println!("{} ({}) -> {} ({})", couple.0, res.1.unwrap().extensions_str()[0], res.0, res.2.extensions_str()[0])
         } else {
@@ -157,16 +163,34 @@ fn process(couples: Vec<(&&String, Option<&&String>)>, i_fmt_s: Option<&String>,
     Ok(())
 }
 
+fn expand_patterns_to_files(patterns: ValuesRef<String>) -> ImcoResult<Vec<String>> {
+    let mut files = Vec::new();
+    for pattern in patterns {
+        match glob(&pattern) {
+            Ok(paths) => {
+                for entry in paths {
+                    match entry {
+                        Ok(path) => files.push(path.to_str().unwrap().to_string()),
+                        Err(e) => return Err(ImcoError::BatchReadEntry(e.to_string())),
+                    }
+                }
+            }
+            Err(e) => return Err(ImcoError::BatchPattern(e.to_string(), pattern.to_string())),
+        }
+    }
+    Ok(files)
+}
+
 fn parse_and_execute(matches: ArgMatches) -> Result<(), ImcoError> {
-    // TODO: Add batch processing
     let batch = matches.get_flag("batch");
 
     let mut couples = vec![];
 
-    let input_files: Vec<&String> = matches
-        .get_many::<String>("input")
-        .unwrap()
-        .collect();
+    let input_files: Vec<String> = if batch {
+        expand_patterns_to_files(matches.get_many::<String>("input").unwrap())?
+    } else {
+        matches.get_many::<String>("input").unwrap().map(|x| {x.to_string()}).collect()
+    };
 
     let output_files: Vec<&String> = matches
         .get_many::<String>("output")
